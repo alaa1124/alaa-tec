a53645e2d37529c17cbbc5687a0f2ddf72c65c56
Mohamed Salah <mohamed.salah@intalio.com>
2024-12-16 20:46:24 +0200

    Update models.py

--- construction_stock/models/models.py
+++ construction_stock/models/models.py
@@ -12,7 +12,7 @@ class StockMove(models.Model):
     _inherit = "stock.move"
     project_id = fields.Many2one('project.project', related='picking_id.project_id')
     item_id = fields.Many2one("project.item", domain="[('id','in',allowed_item_ids)]")
-    item_line = fields.Many2one('project.tender', domain="[('project_id','=?',project_id), ('item_id','=?',item_id)]")
+    item_line = fields.Many2one('project.tender', domain="[('project_id','=?',project_id), ('item','=?',item_id)]")
     allowed_item_ids = fields.Many2many("project.item", "stock_move_item", "item", "id", compute="get_allowed_item_ids")
 
     @api.depends('picking_id.project_id')
