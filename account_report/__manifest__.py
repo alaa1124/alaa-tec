83e0e625cdaf7c04be4a2ad5e78e1d03b2569a32
Tasnim-Talaat <tasnim.talaat23@gmail.com>
2025-01-08 15:47:56 +0200

    upload_module

---
+++ account_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Account Report",
+    "description": "",
+    "depends": ['base', 'account'],
+    "data":[
+         'reports/report.xml',
+
+    ],
+    "assets": {
+    
+    },
+    'application':True,
+}
