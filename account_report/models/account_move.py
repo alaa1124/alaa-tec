0c8d5c2fa8488a634d9871b64e6f4a04e0828d88
Tasnim-Talaat <tasnim.talaat23@gmail.com>
2025-01-08 17:45:55 +0200

    ..

--- account_report/models/account_move.py
+++ account_report/models/account_move.py
@@ -3,11 +3,18 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    @api.onchange('name')
+    def _onchange_name(self):
+        if self.name:
+            for line in self.line_ids:
+                if line.name:
+                    line.name = f"{line.name} - Updated"
+
     def action_post(self):
         res = super(AccountMove, self).action_post()
         for move in self:
             for line in move.line_ids:
-                if line.name: 
+                if line.name:
                     line.name = f"{line.name} - Updated"
 
         return res
