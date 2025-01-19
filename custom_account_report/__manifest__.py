1318a5aa812e4f9da9a271781041052914421b89
ayahali96 <Ayahassan391996@gmail.com>
2025-01-19 16:36:24 +0200

    test-report-font

---
+++ custom_account_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Account Reports',
+    'version': '1.0',
+    'depends': ['account_reports'],
+    'data': [
+        'views/report_templates.xml',  # Include the XML file you created
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_account_report/models/account_report.py
@@ -0,0 +1,12 @@
+from odoo import models
+
+class GeneralLedgerReport(models.Model):
+    _inherit = 'account.report'
+
+    def _get_styles(self):
+        styles = super()._get_styles()
+        # Customize font size
+        styles.update({
+            'font_size': 24,  # Change font size as needed
+        })
+        return styles
