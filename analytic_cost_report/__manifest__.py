2f9277a29ff62021a550db7f3e9e97566909ab13
ayahali96 <Ayahassan391996@gmail.com>
2025-01-21 18:39:24 +0200

    .

--- analytic_cost_report/__manifest__.py
+++ analytic_cost_report/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Analytic Costs Report',
     'depends': ['account'],  
     'data': [
-        'reports/analytic_cost_action.xml',
+        # 'reports/analytic_cost_action.xml',
     ],
     'installable': True,
     'application': False,
