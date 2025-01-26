e712160563b3119035a0dba710738c88896ab577
ayahali96 <Ayahassan391996@gmail.com>
2025-01-26 22:25:33 +0200

    show_backdate_wizard_in_form_view

--- stock_picking_backdate_all/__manifest__.py
+++ stock_picking_backdate_all/__manifest__.py
@@ -1,17 +1,17 @@
 {
     'name': 'Stock Picking Backdate',
-    'version': '17.0.1.0.0', 
+    'version': '17.0.1.0.0',
     'category': 'Extra Tools',
     'author': "10 Orbits",
     'website': "https://www.10orbits.com",
     'summary': 'This module allows you to entry the stock accounting details in backdate.',
     'depends': [
-        'stock','account'
+        'stock', 'account'
     ],
     'data': [
-        'security/security.xml',
         'security/ir.model.access.csv',
         'wizard/change_to_backdate_wiz.xml',
+        'wizard/inherit_stock_view_picking_form.xml',
         'views/stock_picking_backdate_action.xml'
     ],
     'images': ['static/description/Banner.png'],
