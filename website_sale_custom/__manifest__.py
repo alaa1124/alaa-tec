3c9eb410e244b87af4b3881dea838828182bf698
Mohamed Salah <mohamed.salah@intalio.com>
2025-03-24 11:17:10 +0200

    website_sale_custom

--- website_sale_custom/__manifest__.py
+++ website_sale_custom/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Website Sale Custom",
-    'depends': ['website_sale'],
+    'depends': ['website_sale', 'itsys_real_estate'],
     'data': [
-
+        'views/views.xml'
     ],
 
-}  
+}
--- website_sale_custom/models/models.py
+++ website_sale_custom/models/models.py
@@ -4,16 +4,51 @@ from odoo import models, fields, api
 from num2words import num2words
 
 
-class statementline(models.Model):
-    _inherit = 'account.bank.statement.line'
-    recipient_id = fields.Many2one(comodel_name="res.partner", string="المستلم", required=False)
+class ProductTemplate(models.Model):
+    _inherit = 'res.company'
 
-    def amount_to_text(self):
-        convert_amount_in_words = num2words(abs(self.amount), lang='ar')
-        st="فقط "+convert_amount_in_words+" جنية "
-        return st
+    form_file = fields.Binary()
+    form_file_name = fields.Char(string='Form File Name')
 
-    def print_receipt(self):
-        return self.env.ref('receipt_print.receipt_payment_action2').report_action(self, data={}, config=False)
+    terms_and_conditions = fields.Html()
 
 
+class OwnershipContract(models.Model):
+    _inherit = "ownership.contract"
+
+    sale_order_line = fields.Many2one('sale.order.line', ondelete='cascade')
+    sale_order = fields.Many2one(related='sale_order_line.order_id')
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    reservation = fields.One2many('ownership.contract', 'sale_order_line')
+
+    def create_reservation(self):
+        order = self.order_id
+        self.reservation.create({
+            'sale_order_line': self.id,
+            'partner_id': order.partner_id.id,
+            'building_unit': self.product_template_id.id,
+            'date_payment': order.date_order.date()
+        })
+
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        if res.order_id.website_id:
+            res.create_reservation()
+
+        return res
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _cart_update_order_line(self, *args, **kwargs):
+        order = super()._cart_update_order_line(*args, **kwargs)
+        order.write({
+            'product_uom_qty': 1
+        })
+        return order
