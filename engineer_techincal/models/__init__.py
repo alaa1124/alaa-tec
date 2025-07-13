1b488c4fee471d10bc09060ea9a779c968911b24
Samar-Salem <76627054+Samar-Salem@users.noreply.github.com>
2025-07-13 16:49:14 +0300

    Update __init__.py

--- engineer_techincal/models/__init__.py
+++ engineer_techincal/models/__init__.py
@@ -4,7 +4,4 @@ from . import contract
 from . import engineer_techincal
 from . import deduction_allownace_template
 from . import wbs
-<<<<<<< HEAD
-=======
-# from . import engineer_template_report
->>>>>>> 4fc09f2c3290f78e48cb1b80a2fe554177daf137
+
