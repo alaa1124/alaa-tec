cb9265322a7691ec3420d1aeb538a2c4a7d90f93
Mohamed Salah <mohamed.salah.pet@gmail.com>
2025-07-31 00:15:09 +0300

    Squashed commit of the following:
    commit d63e575a5287e897b978713b5c96797c5bc15714
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Thu Jul 31 00:02:56 2025 +0300
    
        Update engineer_techincal.py
    
    commit 7ad39f0ac0750cd71282ace7aad5556c23617128
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Thu Jul 31 00:01:47 2025 +0300
    
        Update deduction_allownace_template.py
    
    commit 226883327e4b72f6d342b107c9cae7dceb91ecff
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Thu Jul 31 00:01:23 2025 +0300
    
        Update deduction_allownace_template.py
    
    commit f7c8d7923959931c5a16eb471ff73f0e9daa0a25
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Wed Jul 30 23:58:24 2025 +0300
    
        Update engineer_techincal.py
    
    commit 7fa650ea762a011dbb189ceccc1c619cf763486a
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Wed Jul 30 23:55:47 2025 +0300
    
        Update deduction_allownace_template.py
    
    commit f1b0ce88e9918126e818a06bc68602744adea179
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Wed Jul 30 23:40:17 2025 +0300
    
        Update deduction_allownace_template.py
    
    commit 006f4f769051c3887ffadc617c1b2c5b3308b7a6
    Author: Mohamed Salah <mohamed.salah.pet@gmail.com>
    Date:   Wed Jul 30 23:35:33 2025 +0300
    
        Update deduction_allownace_template.py

--- engineer_techincal/models/deduction_allownace_template.py
+++ engineer_techincal/models/deduction_allownace_template.py
@@ -9,12 +9,44 @@ class Deduction(models.Model):
                                      string="Type of Contract")
     name = fields.Many2one("contract.deduction.allowance",
                            domain="[('type','=',contract_type),('subtype','=','deduction')]")
-    is_precentage = fields.Boolean()
-    percentage = fields.Float()
+    is_precentage = fields.Boolean(store=True)
+    percentage = fields.Float(store=True)
     value = fields.Float(store=True, compute="onchange_is_precentage")
     counterpart_account_id = fields.Many2one("account.account", related='name.counterpart_account_id')
     amount_total_contract = fields.Float(related='engineer_id.amount_total_without_ded_allowance')
 
+    # @api.depends('engineer_id', 'amount_total_contract', 'name', 'value')
+    # def compute_percentage(rec):
+    #     for self in rec:
+    #         if self.name.calculation_type == 'percentage':
+    #             self.percentage = self.name.amount
+    #             self.is_precentage = True
+    #         else:
+    #             self.percentage = (self.amount_total_contract * 100) / self.value if self.value > 0 else 0
+
+    @api.depends('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
+    def onchange_is_precentage(rec):
+        for self in rec:
+            if self.is_precentage and self.name.calculation_type == 'percentage':
+                self.value = self.amount_total_contract * (self.percentage / 100)
+            else:
+                self.value = self.name.amount
+
+
+class Allownace(models.Model):
+    _name = "engineer.allowance.lines"
+
+    engineer_id = fields.Many2one("project.engineer.techincal")
+    contract_type = fields.Selection([('owner', 'owner'), ('subconstructor', 'subconstructor')], default='owner',
+                                     string="Type of Contract")
+    name = fields.Many2one("contract.deduction.allowance", domain="[('type','=',contract_type),\
+    ('subtype','=','allowance')]")
+    is_precentage = fields.Boolean()
+    percentage = fields.Float()
+    value = fields.Float(store=True, compute="onchange_is_precentage")
+    counterpart_account_id = fields.Many2one("account.account", related='name.counterpart_account_id')
+
+    amount_total_contract = fields.Float(related='engineer_id.amount_total_without_ded_allowance')
 
 
     @api.onchange('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
@@ -67,3 +99,4 @@ class Allownace(models.Model):
                 self.value = self.amount_total_contract * (self.percentage / 100)
             else:
                 self.percentage = (self.amount_total_contract * 100) / self.value if self.value > 0 else 0
+
