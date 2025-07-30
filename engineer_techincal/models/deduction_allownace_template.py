952ae04fed0bcbcaa68cf0ed6e4255b02552fbc6
Mohamed Salah <mohamed.salah.pet@gmail.com>
2025-07-30 23:35:33 +0300

    Update deduction_allownace_template.py

--- engineer_techincal/models/deduction_allownace_template.py
+++ engineer_techincal/models/deduction_allownace_template.py
@@ -11,14 +11,14 @@ class Deduction(models.Model):
                            domain="[('type','=',contract_type),('subtype','=','deduction')]")
     is_precentage = fields.Boolean()
     percentage = fields.Float()
-    value = fields.Float(store=True)
+    value = fields.Float(store=True, compute="onchange_is_precentage")
     counterpart_account_id = fields.Many2one("account.account", related='name.counterpart_account_id')
     amount_total_contract = fields.Float(related='engineer_id.amount_total_without_ded_allowance')
 
 
 
     @api.onchange('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
-    @api.constrains('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
+    @api.depends('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
     def onchange_is_precentage(rec):
         for self in rec:
             if self.name and self.percentage == 0:
@@ -45,23 +45,25 @@ class Allownace(models.Model):
     ('subtype','=','allowance')]")
     is_precentage = fields.Boolean()
     percentage = fields.Float()
-    value = fields.Float()
+    value = fields.Float(store=True, compute="onchange_is_precentage")
     counterpart_account_id = fields.Many2one("account.account", related='name.counterpart_account_id')
 
     amount_total_contract = fields.Float(related='engineer_id.amount_total_without_ded_allowance')
 
 
     @api.onchange('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
-    def onchange_is_precentage(self):
-        if self.name and self.percentage == 0:
-            if self.name.calculation_type == 'percentage':
-                self.percentage = self.name.amount
-                self.is_precentage = True
+    @api.depends('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
+    def onchange_is_precentage(rec):
+        for self in rec:
+            if self.name and self.percentage == 0:
+                if self.name.calculation_type == 'percentage':
+                    self.percentage = self.name.amount
+                    self.is_precentage = True
+                else:
+                    self.value = self.name.amount
+
+            if self.is_precentage == True:
+
+                self.value = self.amount_total_contract * (self.percentage / 100)
             else:
-                self.value = self.name.amount
-
-        if self.is_precentage == True:
-
-            self.value = self.amount_total_contract * (self.percentage / 100)
-        else:
-            self.percentage = (self.amount_total_contract * 100) / self.value if self.value > 0 else 0
+                self.percentage = (self.amount_total_contract * 100) / self.value if self.value > 0 else 0
