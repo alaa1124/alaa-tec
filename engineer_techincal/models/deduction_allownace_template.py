7fa650ea762a011dbb189ceccc1c619cf763486a
Mohamed Salah <mohamed.salah.pet@gmail.com>
2025-07-30 23:55:47 +0300

    Update deduction_allownace_template.py

--- engineer_techincal/models/deduction_allownace_template.py
+++ engineer_techincal/models/deduction_allownace_template.py
@@ -9,30 +9,28 @@ class Deduction(models.Model):
                                      string="Type of Contract")
     name = fields.Many2one("contract.deduction.allowance",
                            domain="[('type','=',contract_type),('subtype','=','deduction')]")
-    is_precentage = fields.Boolean()
-    percentage = fields.Float()
-    value = fields.Float(store=True)
+    is_precentage = fields.Boolean(store=True, compute="compute_percentage")
+    percentage = fields.Float(store=True, compute="compute_percentage")
+    value = fields.Float(store=True, compute="compute_value")
     counterpart_account_id = fields.Many2one("account.account", related='name.counterpart_account_id')
     amount_total_contract = fields.Float(related='engineer_id.amount_total_without_ded_allowance')
 
-
-
-    @api.onchange('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
-    @api.constrains('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
-    def onchange_is_precentage(rec):
+    @api.depends('engineer_id', 'amount_total_contract', 'name', 'value')
+    def compute_percentage(rec):
         for self in rec:
-            if self.name and self.percentage == 0:
-                if self.name.calculation_type == 'percentage':
-                    self.percentage = self.name.amount
-                    self.is_precentage = True
-                else:
-                    self.value = self.name.amount
-
-            if self.is_precentage == True:
-
-                self.value = self.amount_total_contract * (self.percentage / 100)
+            if self.name.calculation_type == 'percentage':
+                self.percentage = self.name.amount
+                self.is_precentage = True
             else:
                 self.percentage = (self.amount_total_contract * 100) / self.value if self.value > 0 else 0
+
+    @api.depends('engineer_id', 'amount_total_contract', 'percentage', 'is_precentage', 'name')
+    def compute_value(rec):
+        for self in rec:
+            if self.is_precentage and self.name.calculation_type == 'percentage':
+                self.value = self.amount_total_contract * (self.percentage / 100)
+            else:
+                self.value = self.name.amount
 
 
 class Allownace(models.Model):
